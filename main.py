e3c98e2d61a51f9153aa77782c4c4461db4863c8
CDwee <96454637+CDwee@users.noreply.github.com>
2022-03-06 15:14:35 -0600

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# Sarted at 1:33 3-6-2022
+
+print("Tried finding a way to get the dimensions right and nothing worked and I can make it all in javascript but that just might take to long")
+
+# Ended at 3:13 3-6-2022
